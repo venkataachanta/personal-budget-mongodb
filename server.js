c79ec1110ee973fae9e38209dc151d9d5c66d6e9
pranavi <vachanta@uncc.edu>
2020-10-07 10:27:47 -0400

    CORS chart

--- server.js
+++ server.js
@@ -1,19 +1,52 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/',express.static('public'));
+app.use(cors());
 
-const newlocation = require('./manifest.json');
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+const budget = {
+    myBudget: 
+    [
+        {
+            title:'Eat out',
+            budget: 50
+        },
+        {   
+            title:'Rent',
+            budget: 675
+        },
+        {
+            title:'Grocery',
+            budget: 110
+        },
+        {
+            title:'Clothing',
+            budget: 75
+        },
+        {
+            title:'Loans',
+            budget: 1000
+        },
+        {
+            title:'Credit Card Bills',
+            budget: 200
+        },
+        {
+            title: 'Gas',
+            budget: 300
+        },
+        {
+            title:'Savings', 
+            budget: 500
+        },
+    ]
+};
 
 app.get('/budget', (req, res) => {
-    res.json(newlocation);
+    res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}');
+    console.log('API served at http://localhost:${port}');
 });
